74e71ae1e7b1baf7206671afd52b5f653f594d84
Igor Menshikov <imenshikov@unison.com>
2012-11-08 16:31:14 +0400

    keep term to riak value conversion logic in one place; help dialyzer

--- include/rp_obj.hrl
+++ include/rp_obj.hrl
@@ -1,3 +1,4 @@
-
--include_lib("riakc/include/riakc_obj.hrl").
--define(CTYPE_BINARY, "application/octet-stream").
+%% -include_lib("riakc/include/riakc_obj.hrl").
+%% -include_lib("riak_pb/include/riak_pb_kv_codec.hrl").
+-define(CTYPE_ERLANG_BINARY, "application/x-erlang-binary").
+-define(CTYPE_BINARY,        "application/octet-stream").
--- src/rp_obj.erl
+++ src/rp_obj.erl
@@ -8,20 +8,34 @@
 
 -include("rp_obj.hrl").
 
-new(B, K, V) when is_binary(V) -> riakc_obj:new(B,K, V, ?CTYPE_BINARY);
-new(B, K, V) -> riakc_obj:new(B, K, V).
+-spec new(binary(), binary(), term()) -> tuple().
+new(B, K, V) ->
+    {CT, Val} = pack_binary(V),
+    riakc_obj:new(B, K, Val, CT).
 
-update_value(O, V) when is_binary(V) -> riakc_obj:update_value(O, V, ?CTYPE_BINARY);
-update_value(O, V) -> riakc_obj:update_value(O, V).    
+-spec update_value(tuple(), term()) -> tuple().
+update_value(O, V) ->
+    {CT, Val} = pack_binary(V),
+    riakc_obj:update_value(O, Val, CT).
 
+-spec get_value(tuple())-> term().
 get_value(O)->
     unpack_binary(O:get_content_type(), O:get_value()).
-         
+
+-spec get_values(tuple())-> [term()].
 get_values(O)->
     [unpack_binary(CT,V) || {CT, V} <- lists:zip(O:get_content_types(), O:get_values())].
-                           
+
 %% --------------------inner funs--------------------
 unpack_binary(?CTYPE_ERLANG_BINARY, V) -> binary_to_term(V);
 unpack_binary(?CTYPE_BINARY, V) -> V;
 unpack_binary(undefined, V) -> binary_to_term(V); %thx to dblayer
 unpack_binary(CT, _V) -> throw({unknown_content_type, CT}).
+
+
+%%explicit doubling of logic from riak_pb_kv_codec
+-spec pack_binary(term())-> {list(), binary()}.
+pack_binary(Value) when is_binary(Value) ->
+    {?CTYPE_BINARY, Value};
+pack_binary(Value) ->
+    {?CTYPE_ERLANG_BINARY, term_to_binary(Value)}.
